349ec739a3b5f9aac486da19d310b1d65126c384
Soumojitshome2023 <soumojitshome2021@gmail.com>
2023-08-07 19:19:15 +0530

    footer removed from home

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import Navbar from "./Navbar";
 import "./About.css";
-import { FaGithub,FaLinkedin } from "react-icons/fa6";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
 import { BiLogoGmail } from "react-icons/bi";
-import bibhab from "./image/bibhab.png"
-import ritam from "./image/ritam.png"
+import Footer from './Footer'
+
 const AboutUsPage = () => {
   const [activeIndex, setActiveIndex] = useState(null);
 
@@ -37,58 +37,40 @@ const AboutUsPage = () => {
 
   return (
     <>
-    <div className='About_bg'>
-      <Navbar/>
-      <div className="whole_about">
-      <div className="about_card_details">
-        <ul>
-        <li>Welcome to our website! We are a passionate team dedicated to providing high-quality fitness services and promoting a healthy lifestyle.</li>
-        <li>Our mission is to help individuals achieve their fitness goals and improve their overall well-being. We offer a wide range of fitness programs, including gym workouts, yoga classes, personal training, and more.</li>
-        <li>With our experienced trainers and state-of-the-art facilities, we strive to create a supportive and motivating environment for our clients. Whether you're a beginner or an advanced fitness enthusiast, we have something for everyone.</li>
-        <li>Join us today and embark on a journey towards a healthier and happier you. We are here to guide and support you every step of the way.</li>
-        <li>Feel free to explore our website and learn more about the services we offer. If you have any questions or need assistance, don't hesitate to reach out to our friendly team.</li>
-        <li>Thank you for choosing us as your fitness partner!</li>
-        </ul>
+      <div className='About_bg'>
+        <Navbar />
+        <div className="whole_about">
+          <div className="about_card_details">
+            <ul>
+              <li>Welcome to our website! We are a passionate team dedicated to providing high-quality fitness services and promoting a healthy lifestyle.</li>
+              <li>Our mission is to help individuals achieve their fitness goals and improve their overall well-being. We offer a wide range of fitness programs, including gym workouts, yoga classes, personal training, and more.</li>
+              <li>With our experienced trainers and state-of-the-art facilities, we strive to create a supportive and motivating environment for our clients. Whether you're a beginner or an advanced fitness enthusiast, we have something for everyone.</li>
+              <li>Join us today and embark on a journey towards a healthier and happier you. We are here to guide and support you every step of the way.</li>
+              <li>Feel free to explore our website and learn more about the services we offer. If you have any questions or need assistance, don't hesitate to reach out to our friendly team.</li>
+              <li>Thank you for choosing us as your fitness partner!</li>
+            </ul>
+          </div>
+
+
+
+          <h2>Frequently Asked Questions</h2>
+          {faqData.map((faq, index) => (
+            <div key={index}>
+              <h3 onClick={() => handleQuestionClick(index)}>{faq.question}</h3>
+              {activeIndex === index && <p>{faq.answer}</p>}
+            </div>
+          ))}
+        </div>
+
+
+
+
+
+        <Footer />
+
+
+
       </div>
-
-      
-
-      <h2>Frequently Asked Questions</h2>
-      {faqData.map((faq, index) => (
-        <div key={index}>
-          <h3 onClick={() => handleQuestionClick(index)}>{faq.question}</h3>
-          {activeIndex === index && <p>{faq.answer}</p>}
-        </div>
-      ))}
-    </div>
-    <div className="bot_about">
-      <h1>Contact Us - Creator of this Site</h1>
-        <div className="contact">
-          <section className='ritam'> 
-              <h1 className='name_con'>Ritam Majumder</h1>
-              <img src={ritam} alt="" />
-              <div className="link_con">
-              <a href="https://github.com/Ritam0" className="github" target='_blank'><FaGithub/></a>
-              <a href="https://www.linkedin.com/in/ritam-majumder-a0795122b/" className="linkdin" target='_blank'><FaLinkedin/></a>
-              <a href="mailto:ritammajumder0@gmail.com" className="email" target='_blank'><BiLogoGmail/></a>
-              </div>
-          </section>
-          <section className='bibhab'> 
-              <h1 className='name_con'>Bibhab  Mukhopadhyay</h1>
-              <img src={bibhab} alt="" />
-              <div className="link_con">
-                <a href="https://github.com/BIBHAB22" className="github" target='_blank'><FaGithub/></a>
-                <a href="https://www.linkedin.com/in/bibhab-mukhopadhyay-898303241/" className="linkdin" target='_blank'><FaLinkedin/></a>
-                <a href="mailto:bibhabindia@gmail.com" className="email" target='_blank'><BiLogoGmail/></a>
-              </div>
-              
-          </section>
-        </div>
-    </div>
-        
-        
-
-    </div>
     </>
   );
 };
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -38,7 +38,7 @@ const Home = () => {
 
 
             <Services />
-            <Footer />
+            {/* <Footer /> */}
 
         </div>
     )
