9b1356db82ef16e97044dd1143e3c4ea2d2e7ac8
Soumojitshome2023 <soumojitshome2021@gmail.com>
2023-08-07 14:04:49 +0530

    home done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,34 +1,40 @@
 import React from 'react'
 import Navbar from './Navbar'
 import './Home.css';
+import home_doctor_pic from "./image/home_doctor_pic.jpg"
 
 const Home = () => {
-  return (
-    <div>
-        <Navbar />
+    return (
+        <div>
+            <Navbar />
 
 
-      <main>
+            <main>
 
-        <section id='left_sec'>
-          hi
-        </section>
-        <section id='right_sec'>
-          <img src="client\src\components\img\home_doctor_pic.jpg" alt="pic" />
-        </section>
-      </main>
+                <section id='left_sec'>
+                    <section id='home_heading'>
+                        Hello. <span> World. </span>
+                    </section>
+                    <section id='home_desc'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda doloribus, rem magni omnis ex ab,
+                    </section>
+                    <section id='home_left_btn'>
+                        <button class="button">
+                            <span class="button-content">Our Services</span>
+                        </button>
+                    </section>
+
+                </section>
+                <section id='right_sec'>
+                    <img src={home_doctor_pic} alt="pic" />
+                </section>
 
 
+            </main>
 
 
-
-
-
-
-
-
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Home
