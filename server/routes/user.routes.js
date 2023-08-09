0736b27bcc8143bdb7f50d68fba3aeb981984733
Ritam Majumder <ritmmajumder0@gmail.com>
2023-08-09 23:23:32 +0530

    feedback and customer reg

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,9 +3,13 @@ import { Router } from "express";
 import { register , login , logout , profile, forgotPassword, resetPassword } from "../controller/user.controller.js";
 import { isLoggedIn } from "../middleware/auth.middleware.js";
 import upload from "../middleware/multer.middleware.js";
+import { customer_register } from "../controller/customer.controller.js";
+import { feedback_take } from "../controller/feedback.controller.js";
 const userRoutes=Router();
 
 userRoutes.post('/register',upload.single("avatar"),register);
+userRoutes.post('/customer/registration', customer_register);
+userRoutes.post('/feedback', feedback_take);
 userRoutes.post('/login',login);
 userRoutes.get('/logout',logout);
 userRoutes.get('/profile',isLoggedIn ,profile);
