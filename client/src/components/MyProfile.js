936769782a4db42335f1bfc02ac91dce0ef5a8e4
Soumojitshome2023 <soumojitshome2021@gmail.com>
2023-08-12 13:56:38 +0530

    my profile update

--- client/src/components/MyProfile.js
+++ client/src/components/MyProfile.js
@@ -1,41 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Navbar from './Navbar';
 import axios from 'axios';
+import './MyProfile.css';
+import Navbar from './Navbar'
 
 const MyProfile = () => {
   const navigate = useNavigate();
   const [userDetails, setUserDetails] = useState({});
   const [uri, setUri] = useState('https://mgcfeni.edu.bd/midea/featuredimage/featuredimage2019-03-04-13-47-19_5c7d1e5732a77.jpg');
-  
 
-  const callProfile = async () => {
-    try {
-      const token = localStorage.getItem('token');
-      const mail = localStorage.getItem('mail');
-      if(!token){
-        navigate('/login');
-      }
-      const response = await axios.get('http://localhost:3001/profile', {
-        params: {
-          mail:mail
-        }
-      });
 
-      const data = response.data.user;
-      console.log(data);
-      setUserDetails(data);
-      setUri(data.avatar.secure_url);
-      
-    } catch (error) {
-      console.error(error);
-      navigate('/login'); // Navigate to login page
-    }
-  };
+  // try {
+  //     const token = localStorage.getItem('token');
+  //     const mail = localStorage.getItem('mail');
+  //     if(!token){
+  //       navigate('/login');
+  //     }
+  //     const response = await axios.get('http://localhost:3001/profile', {
+  //       params: {
+  //         mail:mail
+  //       }
+  //     });
 
-  useEffect(() => {
-    callProfile();
-  }, []);
+  //     const data = response.data.user;
+  //     console.log(data);
+  //     setUserDetails(data);
+  //     setUri(data.avatar.secure_url);
+
+  //   } catch (error) {
+  //     console.error(error);
+  //     navigate('/login'); // Navigate to login page
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   callProfile();
+  // }, []);const callProfile = async () => {
+  //   
 
   const logoutProfile = async (e) => {
     e.preventDefault();
@@ -54,29 +55,48 @@ const MyProfile = () => {
       console.error(error);
     }
   }
-  const ListService=()=>{
+  const ListService = () => {
     navigate("/customer");
   }
 
   return (
-    <div className='main_body'>
-      {/* <Navbar /> */}
-        <div className="main_card">
-          <div className="pic_card">
-            <img src={uri} alt="" className="image" />
+    <>
+      <Navbar />
+      <div className='myproflie_main_body'>
+        <h1>Your Profile</h1>
+        <div className="myprofile_main_card">
+          <div className="myprofile_pic_card">
+            <img src={uri} alt="user_pic" className="image" />
+            <p>Demo Text</p>
           </div>
-          <div className="details">
-            <ul>
-              <li>Name:{userDetails.name}</li>
-              <li>UserId:{userDetails._id}</li>
-              <li>Email:{userDetails.email}</li>
-              <li></li>
-            </ul>
+
+          <div className="myprofile_right_sec">
+
+            <h1>Your Details</h1>
+            <div className="myprofile_details">
+              <ul>
+                <li>Name:{userDetails.name}</li>
+                <li>UserId:{userDetails._id}</li>
+                <li>Email:{userDetails.email}</li>
+                <li></li>
+              </ul>
+            </div>
+            {/* <button id='my_prof_logout_btn' onClick={logoutProfile}>logout</button> */}
+            {/* <button id='my_prof_serv_btn' onClick={ListService}>List Your Service</button> */}
+
+            <div className="myproflie_btns">
+
+              <button class="myproflie_button" onClick={logoutProfile}>
+                <p class="text">logout</p>
+              </button>
+              <button class="myproflie_button" onClick={ListService}>
+                <p class="text">List Your Service</p>
+              </button>
+            </div>
           </div>
-          <button onClick={logoutProfile}>logout</button>
-          <button onClick={ListService}>List Your Service</button>
         </div>
-    </div>
+      </div>
+    </>
   );
 };
 
