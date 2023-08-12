debdaca4bd895911434cd6851fd867e01b16efb2
Soumojitshome2023 <soumojitshome2021@gmail.com>
2023-08-12 16:01:14 +0530

    feedback btn added

--- client/src/components/Medical_store.js
+++ client/src/components/Medical_store.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar'
 import './Medical_store.css';
+import { NavLink } from 'react-router-dom';
 
 const medicalStoresData = [
   { id: 1, name: 'Healthy Pharmacy', location: 'Mukundapur', isOpen: true },
@@ -30,36 +31,37 @@ const MedicalStoresList = () => {
 
   return (
     <>
-    < Navbar />
-    <div className="medical-stores-list-container">
-      <h1>Medical Stores List</h1>
-      <div className="search-filter-container">
-        <input
-          type="text"
-          placeholder="Search by store name"
-          value={searchTerm}
-          onChange={handleSearch}
-        />
-        <select
-          value={isOpenFilter}
-          onChange={handleOpenFilterChange}
-        >
-          <option value="all">All</option>
-          <option value="open">Open</option>
-          <option value="closed">Closed</option>
-        </select>
+      < Navbar />
+      <div className="medical-stores-list-container">
+        <h1>Medical Stores List</h1>
+        <div className="search-filter-container">
+          <input
+            type="text"
+            placeholder="Search by store name"
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+          <select
+            value={isOpenFilter}
+            onChange={handleOpenFilterChange}
+          >
+            <option value="all">All</option>
+            <option value="open">Open</option>
+            <option value="closed">Closed</option>
+          </select>
+        </div>
+        <ul className="stores-list">
+          {filteredStores.map((store) => (
+            <li key={store.id} className="store-item">
+              <h3>{store.name}</h3>
+              <p>Location: {store.location}</p>
+              <p>Status: {store.isOpen ? 'Open' : 'Closed'}</p>
+              <button className='doctors_btn'>Order</button>
+            </li>
+          ))}
+        </ul>
+        <button className='doctors_btn'><NavLink className="doctors_navlink" to="/Feedback">Feedback</NavLink></button>
       </div>
-      <ul className="stores-list">
-        {filteredStores.map((store) => (
-          <li key={store.id} className="store-item">
-            <h3>{store.name}</h3>
-            <p>Location: {store.location}</p>
-            <p>Status: {store.isOpen ? 'Open' : 'Closed'}</p>
-            <button className='doctors_btn'>Order</button>
-          </li>
-        ))}
-      </ul>
-    </div>
     </>
   );
 };
