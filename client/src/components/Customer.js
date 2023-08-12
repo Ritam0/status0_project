1b2afdacdd5fbc1bd332c4b964f1a6d7fa60bddb
Ritam Majumder <ritmmajumder0@gmail.com>
2023-08-13 03:36:42 +0530

    hey

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -50,7 +50,7 @@ function CustomerRegistration({ type }) {
         </section>
         <section id='reg_right_sec'>
           <div className="registration-form-container">
-            <h1>Register {type}</h1>
+            <h1>Register {type}</h1> 
             <form onSubmit={handleSubmit}>
               <div className="form-control">
                 <input className="reg_input reg_input-alt" placeholder="Name" type="text" value={name} onChange={(e) => setName(e.target.value)} required />
